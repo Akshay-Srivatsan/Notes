f7480c5e51d1da6410741e4b3497d542b0619405
Akshay-Srivatsan <srivatsan.akshay@gmail.com>
2016-01-04 21:33:59 -0800

    Added a CollectionViewController to show all the notes for a specific subject

---
+++ Notes/Notes/NoteCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  NoteCollectionViewCell.swift
+//  Notes
+//
+//  Created by Akshay Srivatsan on 1/4/16.
+//  Copyright © 2016 Aaron Brown and Akshay Srivatsan. All rights reserved.
+//
+
+import UIKit
+
+class NoteCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
